a03ea82f43b4fe4cf4ad240e03f400f100e96d42
Håkon Østerbø <hauk88@gmail.com>
2023-04-10 20:32:28 +0200

    add horizontal lines to search for edge

--- extract_cards.py
+++ extract_cards.py
@@ -29,12 +29,37 @@ img_array = np.array(image, dtype=np.uint8)
 
 cords = (start_w,start_h,start_w+card_w,start_h+card_h)
 
+top_h = 188
+bottom_h = 400
 
-vert_line1 = start_h + int(card_h/3)
-vert_line2 = start_h + int(2*card_h/3)
+vert_line1 = start_h + int(top_h/2)
+vert_line2 = start_h + card_h - int(bottom_h/2)
 
-crop1 = img_array[:,vert_line1]
-crop2 = img_array[:,vert_line2]
+h = 1
+
+start_boarder_w = start_w - card_boarder
+end_boarder_w = start_w + card_w + card_boarder
+
+
+crop1 = img_array[vert_line1:vert_line1+h, start_boarder_w:end_boarder_w]
+crop2 = img_array[vert_line2:vert_line2+h, start_boarder_w:end_boarder_w]
+
+threshold = 200
+_, idx = np.where(crop1 > threshold)
+
+v_left_top = idx[0]-1
+v_right_top = idx[-1]+1
+
+threshold = 200
+_, idx = np.where(crop2 > threshold)
+
+v_left_bottom = idx[0]-1
+v_right_bottom = idx[-1]+1
+
+
+print((v_left_top, v_left_bottom))
+
+print((v_right_top, v_right_bottom))
 
 
 im1 = Image.fromarray(crop1)
